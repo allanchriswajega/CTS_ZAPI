c7820d3cefe844b5a0531d75ae2ad1708f1f5fba
Chris Allan Wajega <acwajega@gmail.com>
2016-05-01 10:39:23 +0300

    Wacsoft

--- app.js
+++ app.js
@@ -212,7 +212,7 @@ io.sockets.on('connection', function(socket){
 
         setInterval(function () {
             io.sockets.emit('send_plane_data','now')  ;
-        }, 1000);
+        }, 10000);
 
 
         //--------------------------END OF CODE------------------------------------------------------
